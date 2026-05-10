2db3f34c096902c653ea59d838324f06977c513d
Quisber <200005173+Quisber@users.noreply.github.com>
2026-05-10 19:55:35 +1000

    Загружены тренировочные данные в формате JSONL; добавлен предварительный файл для дообучения

--- engine/training.py
+++ engine/training.py
@@ -1,1 +1,67 @@
 # файл для дообучения
+from unsloth import FastLanguageModel
+import torch
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from datasets import load_dataset
+
+max_seq_length = 2048 
+model_name = "unsloth/meta-llama-3.1-8b-bnb-4bit"
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = model_name,
+    max_seq_length = max_seq_length,
+    load_in_4bit = True,
+    cache_dir = "models/base_model"
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 16,
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 32,
+    lora_dropout = 0.05, 
+    bias = "none",    
+    use_gradient_checkpointing = "unsloth",
+)
+
+def formatting_prompts_func(examples):
+    instructions = examples["instruction"]
+    inputs       = examples["input"]
+    outputs      = examples["output"]
+    texts = []
+    for instruction, input, output in zip(instructions, inputs, outputs):
+        text = f"### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n{output}"
+        texts.append(text)
+    return { "text" : texts, }
+
+dataset = load_dataset("json", data_files={"train": "data/train.jsonl", "test": "data/test.jsonl"})
+dataset = dataset.map(formatting_prompts_func, batched = True)
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset["train"],
+    eval_dataset = dataset["test"],
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    args = TrainingArguments(
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 5,
+        max_steps = 60,
+        learning_rate = 2e-4,
+        fp16 = not torch.cuda.is_bf16_supported(),
+        bf16 = torch.cuda.is_bf16_supported(),
+        logging_steps = 1,
+        output_dir = "models/checkpoints",
+        save_strategy = "steps",
+        evaluation_strategy = "steps",
+    ),
+)
+
+trainer.train()
+
+model.save_pretrained("models/llama_official_style_lora")
+tokenizer.save_pretrained("models/llama_official_style_lora")
