cd8177cd7acd5309e5b561a39cbd54ccbcebff6c
Quisber <200005173+Quisber@users.noreply.github.com>
2026-05-12 18:52:21 +1000

    Добавлен рассчет BERTScore, удалены ненужные файлы и папки

--- NLPmetrics/NLP_evaluator.py
+++ NLPmetrics/NLP_evaluator.py
@@ -1,2 +1,26 @@
 # будет создан вспомогательный метод оценки валидности выходных данных
 # планируются измерения по показателям BERTScore
+from bert_score import score
+import pandas as pd
+
+files = {
+    "Simple Prompt": "NLPmetrics/Simple_based.txt",
+    "Prompt with examples": "NLPmetrics/Prompt_based.txt",
+    "Fine-tuning (OSA)": "NLPmetrics/OSA.txt"
+}
+
+with open("NLPmetrics/Original.txt", "r", encoding="utf-8") as f:
+    reference = [f.read()]
+
+results = []
+
+for label, filename in files.items():
+    with open(filename, "r", encoding="utf-8") as f:
+        candidate = [f.read()]
+    
+    P, R, F1 = score(candidate, reference, lang="en", verbose=False)
+    
+    results.append({"Model": label, "F1-Score": F1.item()})
+
+df = pd.DataFrame(results)
+print(df.to_string(index=False))
